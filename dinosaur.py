3e266d36a8eddbd39d9a38067db1462d7dcf984b
cjones0429 <cejones1997@aim.com>
2021-05-14 13:28:52 -0400

    initial

---
+++ dinosaur.py
@@ -0,0 +1,6 @@
+class Dinosaur:
+    def __init__(self, type, energy, attack_power, health):
+        self.type = type
+        self.energy = energy
+        self.attack_power = 120
+        self.health = 100
---
+++ robot.py
@@ -0,0 +1,6 @@
+class Robot:
+    def __init__(self, name, power_level, health, weapon):
+        self.name = name
+        self.power_level = 150
+        self.health = 100
+        self.weapon = weapon
