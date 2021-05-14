84687255433eb72cd32e7bd04207b9a3c9980eb5
cjones0429 <cejones1997@aim.com>
2021-05-14 13:34:26 -0400

    added herd and fleet files

--- dinosaur.py
+++ dinosaur.py
@@ -2,5 +2,5 @@ class Dinosaur:
     def __init__(self, type, energy, attack_power, health):
         self.type = type
         self.energy = energy
-        self.attack_power = 120
+        self.attack_power = attack_power
         self.health = 100
--- robot.py
+++ robot.py
@@ -1,6 +1,6 @@
 class Robot:
     def __init__(self, name, power_level, health, weapon):
         self.name = name
-        self.power_level = 150
+        self.power_level = power_level
         self.health = 100
         self.weapon = weapon
